ac3ed1d90f0e70af48ddb4c1fd54c7993a2b07d0
Rahul Kumar <67073807+Rahul-Chahar@users.noreply.github.com>
2024-03-28 08:30:34 +0000

    investigation

---
+++ Js_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "rahul@google.com"
+var accountPassword = "12345"
+accountCity = "Mathura"
+
+// accountId =2  (not allowed)
+//console.log(accountId,accountEmail,accountPassword,accountCity)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
