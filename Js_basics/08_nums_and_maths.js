9904ae5bbb3eb5a5f22fc2d78440c30c5e26d86a
Rahul Kumar <67073807+Rahul-Chahar@users.noreply.github.com>
2024-03-29 06:41:34 +0000

    Ludo

--- Js_basics/08_nums_and_maths.js
+++ Js_basics/08_nums_and_maths.js
@@ -23,11 +23,16 @@ const hundreds =100000
 // console.log(Math.min(4,3,6,8));
 // console.log(Math.max(4,3,6,8));
 
-console.log(Math.random()); //Always values b/w in 0 to 1
-console.log(Math.random()*10);
-console.log((Math.random()*10)+1);
+// console.log(Math.random()); //Always values b/w in 0 to 1
+// console.log(Math.random()*10);
+// console.log((Math.random()*10)+1);
 
-const min =10
-const max =20
+// const min =10
+// const max =20
 
-console.log(Math.floor(Math.random() * (max-min+1)) + min)
+// console.log(Math.floor(Math.random() * (max-min+1)) + min)
+
+//Ludo
+const min =1;
+const max =6;
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
