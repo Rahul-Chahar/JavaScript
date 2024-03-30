0a58d4ac561c9ea65e21b57fd9f3373adb91c450
Rahul Kumar <67073807+Rahul-Chahar@users.noreply.github.com>
2024-03-30 09:29:00 +0000

    if

---
+++ Js_control_flow/01_if.js
@@ -0,0 +1,9 @@
+// if
+// const isUserLoggedIn = true
+// const temperature =37
+// if(temperature<50){
+//     console.log("less than 50");
+// } else{
+// console.log("temperature is grater than 50");
+// }
+
